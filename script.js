e5c2422134d4594219b32b5e0d9dad12a9ff7b5d
Harshal Bhute <harshalbhute39@gmail.com>
2024-10-27 14:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 function dateTime(){
 			const timer = document.getElementById('timer');
 			const now = new Date();
-            const day = String(now.getDate()).padStart(2, '0');
-            const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+            const day = now.getDate();
+            const month = now.getMonth() + 1; // Months are zero-indexed
             const year = now.getFullYear();
-            const hours = String(now.getHours()).padStart(2, '0');
-            const minutes = String(now.getMinutes()).padStart(2, '0');
-            const seconds = String(now.getSeconds()).padStart(2, '0');
+            const hours = now.getHours();
+            const minutes = now.getMinutes();
+            const seconds = now.getSeconds();
 
         timer.textContent = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
 		}
